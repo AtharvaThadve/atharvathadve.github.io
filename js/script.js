26d2bc87957b6969ed8b46a875b25b02c51257be
Atharva Thadve <64454972+AtharvaThadve@users.noreply.github.com>
2023-05-16 23:39:16 +0530

    version-0.3
    Editing CSS

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ function guessing_game(){
+    window.open("/prohtml/guessing_game/guessing_game.html");
+  }
--- prohtml/guessing_game/script.js
+++ prohtml/guessing_game/script.js
@@ -1,23 +1,47 @@
 
 window.onload = function () {
+    function getRandomItem(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        const item = arr[randomIndex];
+        return item;
+    }
+    const array1 = ['Are yaar try smaller', 'OOPS SORRY 😟!! Try a Smaller one...', '😭Want smaller...!!', 'Smaller Smaller...'];
+    const smaller = getRandomItem(array1);
+
+    function getRandomItem(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        const item = arr[randomIndex];
+        return item;
+    }
+    const array2 = ['Try bigger as Sun🤣', 'OOPS SORRY 😟!! Try a bigger bro...', '🙁Biggerrrr...!!😔', 'Want Bigger and Bigger😓...'];
+    const bigger = getRandomItem(array2);
+
+    function getRandomItem(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        const item = arr[randomIndex];
+        return item;
+    }
+    const array3 = ['🤗Many Congrats my Hero as no of guess:', '😃Well guessed !! Guessing no is :', '😺Really Nice !! Guesses required :', '😄Congratulations Hero with guesses no:'];
+    const correct = getRandomItem(array3);
+
     const num = Math.floor(Math.random() * 100);
     var guess = 1;
     console.log(num);
     document.getElementById("submitguess").onclick = function () {
         var x = document.getElementById("guessField").value;
         if (num == x) {
-            document.getElementById("demo").innerHTML = "Congrats !! You guess it right with Score :"
+            document.getElementById("demo").innerHTML = correct
                 + guess;
             console.log("Correct");
         }
         else if (num < x) {
             guess++;
-            document.getElementById("demo").innerHTML = "OOPS SORRY!! Try a Smaller one...";
+            document.getElementById("demo").innerHTML = smaller;
             console.log("Bigger");
         }
         else {
             guess++;
-            document.getElementById("demo").innerHTML = "OOPS SORRY!! Try a grater one...";
+            document.getElementById("demo").innerHTML = bigger;
             console.log("Samller");
         }
 
