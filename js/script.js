b6fe6ef339a6b7eff173a7846e7c5183f97f1aa3
Atharva Thadve <64454972+AtharvaThadve@users.noreply.github.com>
2023-05-18 19:29:47 +0530

    version
    CSS Editing

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function darkMode() {
  function guessing_game(){
     window.open("/prohtml/guessing_game/guessing_game.html");
   }
+
+  function myFunction() {
+   var x = document.getElementById("myTopnav");
+   if (x.className === "topnav") {
+     x.className += " responsive";
+   } else {
+     x.className = "topnav";
+   }
+ }
